4ee762e2a281a3f8ef43d0bd8b2110ab8a69fc90
BastianGeier <mail@bastiangeier.de>
2024-09-07 18:16:51 +0200

    add LICENSE

--- src/AnalogKeys.cpp
+++ src/AnalogKeys.cpp
@@ -1,10 +1,10 @@
 /*
 * AnalogKeys library to integrate an analoge input for easy key integration
-* Developer	: Bastian Geier
-* Date      : 30.12.2023
-* E-mail  	: mail@bastiangeier.de
-* Repo      : https://github.com/RedCali/AnalogKeys.git
-
+  Developer  : Bastian Geier
+  Date       : 30.12.2023
+  E-mail     : mail@bastiangeier.de
+  Repo       : https://github.com/RedCali/AnalogKeys.git
+  
 * Any suggestions are welcome.
 */
 
